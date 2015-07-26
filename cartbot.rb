b0a7b596874013d87de3d64a5412cba9444a75aa
Dan Green <linuxdan@gmail.com>
2015-07-26 16:18:47 +0000

    Initial commit of cartbot.

---
+++ cartbot.rb
@@ -0,0 +1,19 @@
+lib_path = File.expand_path '../lib', __FILE__
+require File.join lib_path, 'keyboard_spy'
+require File.join lib_path, 'wav_player'
+require 'logger'
+
+logger = Logger.new(STDOUT)
+
+spy = KeyboardSpy.new
+
+player = WavPlayer.new
+
+spy.listen do |key|
+	case key
+	when 's'
+		logger.info "playing random wav"
+		player.play_random_wav
+		logger.info "done playing random wav"
+	end
+end
